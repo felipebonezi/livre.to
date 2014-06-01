9a8fa09d7a0d302b068c41277426956c3f59e1f4
Gearlles <gearlles@gmail.com>
2014-06-01 13:50:23 -0300

    Criar material.

--- app/controllers/MaterialController.java
+++ app/controllers/MaterialController.java
@@ -1,13 +1,72 @@
 package controllers;
 
-import play.mvc.Controller;
-import play.mvc.Result;
+import java.util.Map;
 
-import views.html.material.*;
+import models.classes.Material;
+import models.classes.Material.PricePolicy;
+import models.classes.User;
+import models.finders.FinderFactory;
+import models.finders.IFinder;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
+import views.html.material.creatematerial;
+import controllers.AbstractApplication.ControllerKey;
 
 public class MaterialController extends Controller {
 
     public static Result create() {
 	return ok(creatematerial.render(""));
+    }
+
+    public static Result upload() {
+	MultipartFormData body = request().body().asMultipartFormData();
+	FilePart materialFile = body.getFile("materialFile");
+
+	Map<String, String[]> formContent = body.asFormUrlEncoded();
+	String[] price = formContent.get("materialPrice");
+	String[] title = formContent.get("materialTitle");
+	String[] pricePolicy = formContent.get("pricePolicyRadios");
+
+	PricePolicy policy = null;
+	switch (pricePolicy[0]) {
+	case "fixedValueRadio":
+	    policy = PricePolicy.FIXED_VALUE;
+	    break;
+	case "minimumValueRadio":
+	    policy = PricePolicy.MINIMUM_VALUE;
+	    break;
+	case "forFreeRadio":
+	    policy = PricePolicy.FREE;
+	    break;
+	}
+
+	Material material = new Material();
+	material.setTitle(title[0]);
+	material.setPricePolicy(policy);
+	material.setPrice(price[0]);
+	material.setMaterialFile(materialFile.getFile());
+
+	// Vai pegar o autor da sessão, considerando que quem faz o upload são
+	// os autores
+	Http.Session session = session();
+	String auth = session.get(ControllerKey.SESSION_AUTH);
+	if (auth != null && !auth.isEmpty()) {
+	    FinderFactory factory = FinderFactory.getInstance();
+	    IFinder<User> finder = factory.get(User.class);
+	    User user = finder.selectUnique(
+		    new String[] { ControllerKey.SESSION_AUTH },
+		    new Object[] { auth });
+
+	    material.setAuthor(user);
+	    // TODO integrar e configurar o banco
+	    material.save();
+
+	    return ok("File uploaded");
+	} else {
+	    return unauthorized("Usuário não está logado!");
+	}
     }
 }
--- app/models/classes/Material.java
+++ app/models/classes/Material.java
@@ -1,14 +1,53 @@
 package models.classes;
 
+import java.io.File;
+
 import javax.persistence.Entity;
 
 import play.db.ebean.Model;
 
 @Entity
 public class Material extends Model {
-    
-    public String name;
+
+    public enum PricePolicy {
+	FREE, MINIMUM_VALUE, FIXED_VALUE
+    }
+
     public User author;
-    public float price;
+    public String title;
+    public PricePolicy pricePolicy;
+    public String price;
+    public File materialFile;
     
+    public User getAuthor() {
+        return author;
+    }
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public PricePolicy getPricePolicy() {
+        return pricePolicy;
+    }
+    public void setPricePolicy(PricePolicy pricePolicy) {
+        this.pricePolicy = pricePolicy;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    public File getMaterialFile() {
+        return materialFile;
+    }
+    public void setMaterialFile(File materialFile) {
+        this.materialFile = materialFile;
+    }
+
 }
