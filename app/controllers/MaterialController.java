5718e098e623e8c808cbe0b5d62951473f711a45
Nestor <nestor.tmj@gmail.com>
2014-06-12 17:57:49 -0300

    Adição de exclusão de material

--- app/controllers/MaterialController.java
+++ app/controllers/MaterialController.java
@@ -35,77 +35,74 @@ import com.avaje.ebean.Page;
 import controllers.AbstractApplication.FinderKey;
 
 public class MaterialController extends Controller {
-    public static Result GO_MAIN = redirect(routes.MaterialController.list(0,
-	    "name", "asc", ""));
+	public static Result GO_MAIN = redirect(routes.MaterialController.list(0,
+		"name", "asc", ""));
 
-    public static Result create() {
+	public static Result create() {
 	if (!AuthenticationController.isLoggedIn())
-	    return unauthorized(unauthorized.render(""));
+		return unauthorized(unauthorized.render(""));
 	Form<Material> materialForm = form(Material.class);
 	return ok(creatematerial.render(AuthenticationController.getUser(), "",
 		materialForm));
-    }
+	}
 
-    public static Result edit(long id) {
+	public static Result edit(long id) {
 	User user = AuthenticationController.getUser();
 	Material material = Material.find.byId(id);
 
 	if (material != null && UserUtil.isOwner(material, user)) {
-	    Form<Material> materialForm = form(Material.class).fill(material);
+		Form<Material> materialForm = form(Material.class).fill(material);
 
-	    return ok(editmaterial.render(user, "", id, materialForm));
+		return ok(editmaterial.render(user, "", id, materialForm));
 	} else {
-	    return unauthorized(unauthorized
-		    .render("Você não tem permissão para realizar essa ação."));
+		return unauthorized(unauthorized
+			.render("Você não tem permissão para realizar essa ação."));
 	}
-    }
+	}
 
-    public static Result update(Long id) {
+	public static Result update(Long id) {
 	try {
-	    if (AuthenticationController.isLoggedIn()) {
+		if (AuthenticationController.isLoggedIn()) {
 		User user = AuthenticationController.getUser();
 		if (user != null) {
-		    MultipartFormData body = request().body().asMultipartFormData();
-		    Material material = MaterialUtil.generateFromForm(body);
-		    material.setAuthor(user);
-		    material.update(id);
-		    
-		    return list(String.format(
-			    "Material #%d atualizado com sucesso!", id));
+			MultipartFormData body = request().body().asMultipartFormData();
+			Material material = MaterialUtil.generateFromForm(body);
+			material.setAuthor(user);
+			material.update(id);
+			
+			return list(String.format(
+				"Material #%d atualizado com sucesso!", id));
 		}
-	    }
-	    
-	    return unauthorized("Usuário não está logado! Sessão expirada?");
+		}
+		
+		return unauthorized("Usuário não está logado! Sessão expirada?");
 	} catch (FileNotFoundException | IllegalArgumentException
 		| ZeroPagesException e) {
-	    return internalServerError();
+		return internalServerError();
 	}
-    }
-
-    public static Result delete(long id) {
-	User user = AuthenticationController.getUser();
-
-	FinderFactory factory = FinderFactory.getInstance();
-	IFinder<Material> finder = factory.get(Material.class);
-	Material material = finder.selectUnique(new String[] { FinderKey.ID },
-		new Object[] { id });
-
-	if (material == null) {
-	    return badRequest(String.format(
-		    "Material #%d não está cadastrado!", id));
-	} else if (UserUtil.isOwner(material, user) || UserController.isAdmin()) {
-	    return ok(String.format("Material #%d removido com sucesso!", id));
-	} else {
-	    return unauthorized(unauthorized
-		    .render("Você não tem permissão para realizar essa ação."));
 	}
-    }
 
-    public static Result list(int page, String sortBy, String order,
-	    String filter) {
+	public static Result delete(long id) {
+		User user = AuthenticationController.getUser();
+		FinderFactory factory = FinderFactory.getInstance();
+		IFinder<Material> finder = factory.get(Material.class);
+		Material material = finder.selectUnique(new String[] { FinderKey.ID }, new Object[] { id });
+
+		if (material == null) {
+			return list(String.format("Material #%d não está cadastrado!", id));
+		} else if (UserUtil.isOwner(material, user) || UserController.isAdmin()) {
+			material.delete();
+			return list(String.format("Material #%d removido com sucesso!", id));
+		} else {
+			return unauthorized(unauthorized.render("Você não tem permissão para realizar essa ação."));
+		}
+	}
+
+	public static Result list(int page, String sortBy, String order,
+		String filter) {
 	User user = AuthenticationController.getUser();
 	if (user == null)
-	    return unauthorized(unauthorized.render("TESTE"));
+		return unauthorized(unauthorized.render("TESTE"));
 
 	FinderFactory factory = FinderFactory.getInstance();
 	IFinder<Material> finder = factory.get(Material.class);
@@ -118,46 +115,46 @@ public class MaterialController extends Controller {
 
 	return ok(listmaterial.render(AuthenticationController.getUser(), null,
 		pages, sortBy, order, filter));
-    }
+	}
 
-    public static Result list(String message) {
+	public static Result list(String message) {
 	FinderFactory factory = FinderFactory.getInstance();
 	IFinder<Material> finder = factory.get(Material.class);
 
 	return ok(listmaterial.render(AuthenticationController.getUser(),
 		message, finder.page(0, 10, "id", "asc", ""), "id", "asc", ""));
-    }
+	}
 
-    public static Result upload() {
+	public static Result upload() {
 	try {
-	    if (AuthenticationController.isLoggedIn()) {
+		if (AuthenticationController.isLoggedIn()) {
 		User user = AuthenticationController.getUser();
 		if (user != null) {
-		    MultipartFormData body = request().body().asMultipartFormData();
-		    Material material = MaterialUtil.generateFromForm(body);
-		    material.setCreated(new Date());
-		    material.setAuthor(user);
-		    material.save();
-		    
-		    return list("Material enviado com sucesso!");
+			MultipartFormData body = request().body().asMultipartFormData();
+			Material material = MaterialUtil.generateFromForm(body);
+			material.setCreated(new Date());
+			material.setAuthor(user);
+			material.save();
+			
+			return list("Material enviado com sucesso!");
 		}
-	    }
-	    
-	    return unauthorized("Usuário não está logado! Sessão expirada?");
+		}
+		
+		return unauthorized("Usuário não está logado! Sessão expirada?");
 	} catch (FileNotFoundException | IllegalArgumentException
 		| ZeroPagesException e) {
-	    return internalServerError();
+		return internalServerError();
 	}
-    }
+	}
 
-    public static Result renderImage(long id) {
+	public static Result renderImage(long id) {
 	FinderFactory factory = FinderFactory.getInstance();
 	IFinder<Material> finder = factory.get(Material.class);
 	Material material = finder.selectUnique(id);
 	if (material.getMaterialThumbnail() == null)
-	    return ok("http://placehold.it/200x280/336600&text=livre.to");
+		return ok("http://placehold.it/200x280/336600&text=livre.to");
 	else
-	    return ok(material.getMaterialThumbnail()).as("image/png");
-    }
+		return ok(material.getMaterialThumbnail()).as("image/png");
+	}
 
 }
